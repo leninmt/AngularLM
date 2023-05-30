1487d70c9701cc24ac8a2c3dabee5acbf19b3437
leninmt <ljc.montalvo@yavirac.edu.ec>
2023-05-30 09:06:59 -0500

    rutas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductComponent } from './pages/product/product.component';
+import { CategoryComponent } from './pages/category/category.component';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'category', component: CategoryComponent},
+
+ ]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
