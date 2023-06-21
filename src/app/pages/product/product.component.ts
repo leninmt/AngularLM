7cb91e10fc60f31f61df475b2e411ec471f29380
leninmt <ljc.montalvo@yavirac.edu.ec>
2023-06-20 21:39:31 -0500

    Postman

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ProductModel, UpdateProductDto, CreateProductDto } from 'src/app/models/product.model';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -8,74 +10,64 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProductComponent {
 
-constructor (private httpClient:HttpClient){
+  products:ProductModel[] = [];
+
+  selectedProduct: UpdateProductDto = {title:'', price:0, description:''};
+
+ constructor(private productService:ProductService) {
+  this.editProduct();
+ }
+
+ ngOnInit(): void {
+   this.getProducts();
+   //this.getProduct();
+   //this.createProduct();
+   //this.updateProduct();
+   //this.deleteProduct();
+ }
+
+ getProducts(){
+   const url = "https://api.escuelajs.co/api/v1/products";
+   this.productService.getAll().subscribe(
+     response =>{
+       this.products = response;
+       console.log(response);
+     }
+   )
+ }
+ getProduct(id: ProductModel['id'] ){
+   const url = "https://api.escuelajs.co/api/v1/products/id";
+   return this.productService.getOne(id).subscribe(
+     response =>{
+       console.log(response);
+     }
+   )
+ }
+ createProduct(product: CreateProductDto){
+   this.productService.store(product).subscribe(
+     response =>{
+       console.log(response);
+     }
+   )
+ }
+ updateProduct(id: ProductModel['id'], product:UpdateProductDto){
+   this.productService.update(id, product).subscribe(
+     response =>{
+       console.log(response);
+     }
+   )
+ }
+ editProduct(){
+   this.selectedProduct = {title:'', price:0, description:''};
+ }
+
+ deleteProduct(id: ProductModel['id']){
+   this.productService.destroy(id).subscribe(
+     response =>{
+       this.products = this.products.filter(product => product.id != id);
+       console.log(response);
+     }
+   )
+ }
 }
 
-ngOnInit(): void{
-  //this.getProducts();
-  this.getProduct();
-  //this.createProduct();
-  //this.updateProduct();
-  //this.deleteProduct();
-}
-
-getProducts(){
-  const url = 'https://api.escuelajs.co/api/v1/products';
-  const response  = this.httpClient.get(url).subscribe(
-  response =>{
-    console.log(response);
-  }
-  )//haciendo peticion al backend
-  console.log(response);
-}
-
-getProduct(){
-  const url = 'https://api.escuelajs.co/api/v1/products';
-  const response  = this.httpClient.get(url).subscribe(
-  response =>{
-    console.log(response);
-  }
-  )//haciendo peticion al backend
-  console.log(response);
-}
-
-createProduct(){
-  const data ={
-      "title": "zapatos",
-      "price": 40,
-      "description": "zapatos deportivos / Lenin Montalvo",
-      "categoryId": 1,
-      "images": ["https://w0.peakpx.com/wallpaper/733/785/HD-wallpaper-inuyasha-the-final-act-dog-demon-inuyasha-full-moon-anime.jpg"] //una imagen
-  }
-
-  const url = 'https://api.escuelajs.co/api/v1/products';
-  this.httpClient.post(url, data).subscribe(
-  response =>{
-    console.log(response);
-  })//haciendo peticion al backend
-};
-
-updateProduct(){
-  const data ={
-      "title": "zapatos nike new",
-      "price": 90,
-      "description": "zapatos deportivos ADIDAS / LENIN JAHIR",
-  }
-
-  const url = 'https://api.escuelajs.co/api/v1/products/181';
-  this.httpClient.put(url, data).subscribe(
-  response =>{
-    console.log(response);
-  });//haciendo peticion al backend
-};
-
-deleteProduct(){
-  const url = 'https://api.escuelajs.co/api/v1/products/181';
-  this.httpClient.delete(url).subscribe(
-  response =>{
-    console.log(response);
-  }
-  )//haciendo peticion al backend
-
-}
-}
